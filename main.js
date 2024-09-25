9640e3fdc1aca3c938246515a25b78ae8bf2b380
Paul Saletzki <39807566+FractalPaul@users.noreply.github.com>
2024-09-25 12:58:44 -0700

    Updated max value
    Updated max value to 10000 to cover many values.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-for (let i = 1; i <= 5000; i++) {
-    for (let j = 1.0; j <= 5000.0; j++) {
+for (let i = 1; i <= 10000; i++) {
+    for (let j = 1.0; j <= 10000.0; j++) {
         let resultIntFloat = i * j;
         let resultIntInt = i * parseInt(j);
 
